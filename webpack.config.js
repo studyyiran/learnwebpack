f34dd223c76702a211a1f750877141be5bc45031
yiran_aihuishou <yiran.sun@aihuishou.com>
2020-01-04 16:51:48 +0800

    设置mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = (env) => {
     console.log(env)
     return {
+        mode: env.mode,
         entry: {
             bundle: './src/app.js'
         },
